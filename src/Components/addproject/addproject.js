44da52af59924d6b8fd217f46f5326cd471f2aa7
Dylan Callaghan <21831599@sun.ac.za>
2020-08-28 12:23:02 +0200

    Fixed addProject

--- src/Components/addproject/addproject.js
+++ src/Components/addproject/addproject.js
@@ -4,20 +4,34 @@ import {get, post} from '../connection';
 class addProject extends Component {
     constructor(props) {
 		super(props);
-		this.state = {response: "Hi"};
-		get("url").then(resp => {
-			this.setState({response: resp});
-		});
+		this.state = {projname: "", response: "nothing yet"};
+		this.handleChangeProj = this.handleChangeProj.bind(this);
+		this.submit = this.submit.bind(this);
 	}
     render() {
+		/*<label for="dependantproj">Dependenancies:</label>
+				<input type="text" id="dependantproj" name="dependantproj"/><br></br>
+				*/
         return (
             <>
-                <label for="projname">Project Name;</label>
-                <input type="text" id="projname" name="projname"/><br></br>
-                <label for="dependantproj">Dependenancies:</label>
-                <input type="text" id="dependantproj" name="dependantproj"/><br></br>
-            </>
+                <label for="projname">Project Name</label>
+                <input type="text" id="projname" name="projname" 
+			value={this.state.projname} onChange={this.handleChangeProj}/><br></br>
+            	<button onClick={this.submit}>Submit</button>
+				<p>{this.state.response}</p>
+			</>
         )
     }
+
+	submit() {
+		post("project/create", ["name"], [this.state.projname]).then(resp => {
+			console.log(resp);
+			this.setState({response: "Received"});
+		});
+	}
+
+	handleChangeProj(event) {
+		this.setState({projname: event.target.value});
+	}
 }
 export default addProject;
