ffee55ebe89ee1f96d1eb0362014523598a8be8c
Dylan Callaghan <21831599@sun.ac.za>
2020-08-28 11:25:34 +0200

    Added route for creation

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
       <Router>
           <Switch>  
             <Route path='/test' component={Test}/>
+            <Route path='/create' component={Test}/>
             <Route path='/' component={Login}/>
             <Route path="*" component={() => "404 NOT FOUND"} />
           </Switch>
